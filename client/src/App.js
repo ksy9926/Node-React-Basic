5be809f4c8eb33266e92137fbde93e4766d3cc4d
수영 김 <ksy9926@naver.com>
2021-04-18 23:59:44 +0900

    Add: Client, Server folder
    start project with basic folders

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
